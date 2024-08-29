8cb9a74a1c4189668128f838214dffacca0eb691
Ali M. Yavari <ali.m.yavari70@gmail.com>
2024-08-29 13:02:35 +0330

    Add assertion for lists with and without order

--- tests/Unit/ArrayTest.php
+++ tests/Unit/ArrayTest.php
@@ -2,12 +2,24 @@
 
 namespace Tests\Unit;
 
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+use Tests\DataProvider\ArrayDataProvider;
 use Tests\TestCase;
 
 class ArrayTest extends TestCase
 {
-    public function test_new(): void
+    #[DataProviderExternal(ArrayDataProvider::class, 'listArray')]
+    public function test_list_order_matter($arr1, $arr2): void
     {
-        $this->assertTrue(true);
+        $this->assertSame($arr1, $arr2);
+    }
+
+    #[DataProviderExternal(ArrayDataProvider::class, 'listArray')]
+    public function test_list_order_not_matter($arr1, $arr2): void
+    {
+        sort($arr1);
+        sort($arr2);
+        
+        $this->assertSame($arr1, $arr2);
     }
 }
