a504f570b69feb17e95a8cc7b89ecebfd986278f
Ali M. Yavari <ali.m.yavari70@gmail.com>
2024-08-29 15:04:19 +0330

    Add assertions for list of associative arrays with and without order

--- tests/Unit/ArrayTest.php
+++ tests/Unit/ArrayTest.php
@@ -31,4 +31,37 @@ class ArrayTest extends TestCase
         
         $this->assertSame($arr1, $arr2);
     }
+
+    #[DataProviderExternal(ArrayDataProvider::class, 'listOfAssociativeArrays')]
+    public function test_list_of_associative_arrays_order_matter($arr1, $arr2): void
+    {
+        foreach ($arr1 as &$item) {
+            ksort($item);
+        }
+        foreach ($arr2 as &$item) {
+            ksort($item);
+        }
+        
+        $this->assertSame($arr1, $arr2);
+    }
+
+    #[DataProviderExternal(ArrayDataProvider::class, 'listOfAssociativeArrays')]
+    public function test_list_of_associative_arrays_order_not_matter($arr1, $arr2): void
+    {
+        foreach ($arr1 as &$item) {
+            ksort($item);
+        }
+        foreach ($arr2 as &$item) {
+            ksort($item);
+        }
+
+        usort($arr1, function ($a, $b) {
+            return strcmp(serialize($a), serialize($b));
+        });
+        usort($arr2, function ($a, $b) {
+            return strcmp(serialize($a), serialize($b));
+        });
+        
+        $this->assertSame($arr1, $arr2);
+    }
 }
